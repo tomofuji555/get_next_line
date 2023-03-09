d0fcb881713840f28db03ab25524a8cc9962eeb0
tomofuji555 <tomokifujiwara321@outlook.jp>
2023-03-09 23:53:49 +0900

    leak

--- get_next_line.c
+++ get_next_line.c
@@ -14,9 +14,10 @@
 
 static char  *free_null (char *buf)
 {
+    free (buf);
     while (*buf)
     {
-        *buf = 0;
+        *buf = '\0';
         buf++;
     }
     return (NULL);
@@ -25,6 +26,7 @@ static char  *free_null (char *buf)
 static char *get_store (char *save, int fd)
 {
     char    *buf;
+    char    *temp;
     int     size;
 
     buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
@@ -34,23 +36,26 @@ static char *get_store (char *save, int fd)
     {
         size = read (fd, buf, BUFFER_SIZE); //bufにreadしたものが保存されている
         if (size == 0)
-            return (NULL);
-        if (size == -1)
             break;
-        buf[size] = '\0';
-        save = ft_strjoin (save, buf);//saveにbufをくっつけ、それをsaveに保存
-        free (buf);
+        if (size == -1)
+            return (free_null (buf));
+        temp = ft_strjoin (save, buf);//saveにbufをくっつけ、それをsaveに保存
+        if (!temp)
+            return (free_null (buf));
+        free_null (buf);
+        buf = temp;
     }
-    if (size == -1)
-        return (free_null (buf));
+    free_null (buf);
     return (save);
 }
 
 static char *put_line (char *save)
 {
-    size_t  count;
+    int     count;
     char    *line;
 
+    if (!save)
+        return (NULL);
     count = 0;
     while (save[count] && save[count] != 'n')
         count++;
@@ -60,15 +65,18 @@ static char *put_line (char *save)
     if (!line)
         return (free_null(save));
     line[count + 1] = '\0';
+    free_null (save);
     return (line);
 }
 
 static char *save_prepare (char *save)
 {
-    size_t  max;
-    size_t  count;
-    char    *save;
+    int     max;
+    int     count;
+    char    *temp;
 
+    if (!save)
+        return (NULL);
     count = 0;
     max = ft_strlen (save);
     save += 1;
@@ -76,10 +84,11 @@ static char *save_prepare (char *save)
         count++;
     if (save[count] == 'n')
         count++;
-    save = ft_substr (save, count, max - count + 1);
-    if (!save)
-        return (free_null (save));
-    save[max - count] = '\0';
+    temp = ft_substr (save, count, max - count);
+    free_null (save);
+    if (!temp)
+        return (NULL);
+    save = temp;
     return (save);
 }
 
@@ -91,8 +100,6 @@ char    *get_next_line (int fd)
     if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
     save = get_store (fd, save);//BUFFER_SIZEをreadする&&saveに保存
-    if (!save)
-        return (NULL);
     line = put_line(save); //出力するもの
     save = save_prepare(save); //前のsaveを預かる
     return (line);
--- get_next_line.h
+++ get_next_line.h
@@ -20,4 +20,9 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+size_t	ft_strlen (const char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 #endif
